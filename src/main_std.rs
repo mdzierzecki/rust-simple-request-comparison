0403a1ab79f0072e686c42ae3edc4f458e7da3a6
Mateusz Dzierzecki <mateusz@dzierzecki.com>
2020-09-06 16:29:16 +0200

    add standard blocking way of fetching data

---
+++ src/main_std.rs
@@ -0,0 +1,51 @@
+#[macro_use]
+extern crate log;
+extern crate simple_logger;
+
+use std::time::{Duration, Instant};
+use futures::prelude::*;
+use tokio::prelude::*;
+use tokio::task;
+use log::*;
+
+
+fn slowwly(delay_ms: u32) -> reqwest::Url {
+    let url = format!(
+        "http://slowwly.robertomurray.co.uk/delay/{}/url/http://www.google.co.uk",
+        delay_ms,
+    );
+    reqwest::Url::parse(&url).unwrap()
+}
+
+async fn request(n: usize) -> Result<(), ()> {
+    reqwest::get(slowwly(1000)).await;
+    info!("Got response from {}", n);
+    Ok(())
+}
+async fn app() -> Result<(), ()> {
+    info!("Starting program!");
+    let a = request(1);
+    let b = request(2);
+
+    a.await;
+    b.await;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let start = Instant::now();
+    simple_logger::init_with_level(log::Level::Debug).unwrap();
+
+    let a = app().await;
+    match a {
+        Ok(_) => info!("Done"),
+        Err(_) => error!("An error ocurred"),
+    }
+
+    let duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+
+}
+
+
